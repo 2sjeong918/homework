e3b2c4f6744fc33325d266aa2a6222f69d466e10
2sjeong918 <2sjeong918@gmail.com>
2017-10-13 21:20:30 +0900

    canvas animation, interaction

--- canvas/canvasactive.js
+++ canvas/canvasactive.js
@@ -40,7 +40,33 @@ var c = canvas.getContext('2d');
 // }
 
 
-// 애니메이션
+// 인터렉션
+
+var mouse = {
+  x: undefined,
+  y: undefined
+}
+var maxRadius = 40;
+var minRadius = 2;
+
+var colorArray = [
+  '#2E112D',
+  '#540032',
+  '#820333',
+  '#C9283E',
+  '#F0433A'
+]
+
+window.addEventListener('mousemove',
+  function (event) {
+  mouse.x = event.x
+  mouse.y = event.y
+})
+
+window.addEventListener('resize', function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+})
 
 function Circle(x, y, dx, dy, radius) {
   this.x = x;
@@ -48,13 +74,16 @@ function Circle(x, y, dx, dy, radius) {
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.minRadius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
   this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     c.strokeStyle = 'blue';
-    c.stroke();
-    // c.fill();
+    // c.stroke();
+    c.fillStyle = this.color
+    c.fill();
   }
   this.update = function () {
     if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
@@ -66,22 +95,35 @@ function Circle(x, y, dx, dy, radius) {
 
     this.x += this.dx;
     this.y += this.dy;
+    
+    // interactivity
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50
+        &&mouse.y -this.y < 50 && mouse.y - this.y > -50) {
+      this.radius += 1;
+      if(this.radius < maxRadius){
+        this.radius += 1;
+      }
+    } else if(this.radius > this.minRadius) {
+      this.radius -= 1;
+    }
 
     this.draw();
   }
 }
-
-
 var circleArray = [];
 
-for (var i = 0; i < 100; i++) {
-  var radius = 30;
-  var x = Math.random() * (innerWidth - radius *2) + radius;
-  var y = Math.random() * (innerHeight - radius *2) + radius;
-  // dx, dy는 속도를 조절함
-  var dx = (Math.random() - 0.5)*8;
-  var dy = (Math.random() - 0.5)*8;
-  circleArray.push(new Circle(x, y, dx, dy, radius));
+function init() {
+  circleArray = [];
+
+  for (var i = 0; i < 600; i++) {
+    var radius = Math.random() * 3 + 1;
+    var x = Math.random() * (innerWidth - radius *2) + radius;
+    var y = Math.random() * (innerHeight - radius *2) + radius;
+    // dx, dy는 속도를 조절함
+    var dx = (Math.random() - 0.5)*3;
+    var dy = (Math.random() - 0.5)*3;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+  }
 }
 
 function animate() {
@@ -108,4 +150,5 @@ function animate() {
     // y += dy;
   }
 
+  init();
   animate();
